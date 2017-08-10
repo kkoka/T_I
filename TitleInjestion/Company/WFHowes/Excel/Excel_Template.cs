b5a7fdd2bfa9a3dc3c048605f71193560575edce
kkoka <30472042+kkoka@users.noreply.github.com>
2017-08-10 15:09:04 -0400

    initial checkin

---
+++ TitleInjestion/Company/WFHowes/Excel/Excel_Template.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace TitleInjestion.Company.WFHowes.Excel
+{
+    public class Excel_Template
+    {
+        public string FileName { get; set; }
+
+        public string FileType { get; set; }
+
+        public string PubID { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Delta_New { get; set; }
+
+        public string Title { get; set; }
+
+        public string OriginalTitle { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string OriginalSubTitle { get; set; }
+
+        public string EditionNumber { get; set; }
+
+        public string DigitalFormat { get; set; }
+
+        public string Authors { get; set; }
+
+        public string Narrators { get; set; }
+
+        public string Illustrator { get; set; }
+
+        public string Editor { get; set; }
+
+        public string Reader { get; set; }
+
+        public string CorporateAuthors { get; set; }
+
+        public string Translator { get; set; }
+
+        public string Photographer { get; set; }
+
+        public string Unabridged { get; set; }
+
+        public string EditionTypeCode { get; set; }
+
+        public string BISAC { get; set; }
+
+        public string Genre1 { get; set; }
+
+        public string Genre2 { get; set; }
+
+        public string Category { get; set; }
+
+        public string SubCategory { get; set; }
+        public string MinAge { get; set; }
+
+        public string PublishDate { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string TotalRunTime { get; set; }
+        public string Language { get; set; }
+
+        public string Fiction { get; set; }
+
+        public string DRM_Flag { get; set; }
+
+        public string MediaType { get; set; }
+
+        public string LibraryPrice_USD { get; set; }
+
+        public string LibraryPrice_GBP { get; set; }
+
+        public string LibraryPrice_EUR { get; set; }
+
+        public string LibraryPrice_AUD { get; set; }
+
+        public string Description { get; set; }
+
+        public string SeriesName { get; set; }
+
+        public string OriginalSeriesName { get; set; }
+
+        public string SeriesPositionNumber { get; set; }
+
+        public string Status { get; set; }
+
+        public string PageCount { get; set; }
+
+        public string RightsCountry { get; set; }
+
+        public string Agent_ParentPublisherName { get; set; }
+
+        public string Agent_PartnerID { get; set; }
+
+        public string PublisherName { get; set; }
+
+        public string ImprintName { get; set; }
+
+        //public string SAP_Status { get; set; }
+        //public string Agent_AccountID { get; set; }
+        //public string Publisher_PartnerID { get; set; }
+        //public string Imprint_PartnerID { get; set; }
+
+
+
+
+    }
+}
